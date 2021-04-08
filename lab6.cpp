369a7ca03083d3fef4533d22a8f6b9744eef54f7
Zach Wadhams <33103466+zwadhams@users.noreply.github.com>
2021-04-07 19:37:04 -0600

    Updated with final code

--- lab6.cpp
+++ lab6.cpp
@@ -72,7 +72,7 @@ int NumberWomen406(vector<Person> ps) {
 
 int NumberOver50_406(vector<Person> ps) {
     int num = 0;
-    for (int i = 0; 9 < (int)ps.size(); i++) {
+    for (int i = 0; i < (int)ps.size(); i++) {
         if (ps[i].Age() > 50 && ps[i].AreaCode() == "406") {
             ++num;
         }
@@ -93,30 +93,37 @@ int main(void) {
     while (inpst >> p) { 
         people.push_back(p);
     }
-    //
+    
     //Find and printnumber of women in list
     int numwomen = NumberWomen(people);
-    cout << "Number of women: " << numwomen;
+    cout << "Number of women: " << numwomen << endl;
+
     //Find and print number of people over 50 in the list
     int numOver50 = NumberPeopleOver50(people);
-    cout << "Number of people over 50: " << numOver50;
+    cout << "Number of people over 50: " << numOver50 << endl;
+
     //Find and print number of women with 406 AC in list
     int women406 = NumberWomen406(people);
-    cout << "Number of women with 406 area code: " << women406;
+    cout << "Number of women with 406 area code: " << women406 << endl;
+
     //Find and print number of people over 50 with 406 AC in list
     int over50_406 = NumberOver50_406(people);
-    cout << "Number of omen over 50 with 406 area code: " << over50_406;
+    cout << "Number of omen over 50 with 406 area code: " << over50_406 << endl;
+
     //Print percentage of women in list with 406 AC
-    cout << setprecision(2) << fixed;
+    cout << setprecision(2) << fixed << endl;
     double percent_women = 0.0;
-    percent_women = (double)numwomen / (double)women406;
-    cout << "Percentage of women with 406 area code: " << percent_women;
+    percent_women = ((double)numwomen / (double)women406) * 100;
+    cout << "Percentage of women with 406 area code: " << percent_women << endl;
+
     //Print percentage of over 50 in list with 406 AC
     double percent50_406 = 0.0;
-    percent50_406 = (double)over50_406 / (double)numOver50;
-    cout << "Percentage of people over 50 with 406 area code: " << percent50_406;
-    cout << "end program";
+    percent50_406 = ((double)over50_406 / (double)numOver50) * 100;
+    cout << "Percentage of people over 50 with 406 area code: " << percent50_406 << endl;
+  
+    cout << "end program" << endl;
 
-
-
+    inpst.close();
+    
+    return 0;
 }
